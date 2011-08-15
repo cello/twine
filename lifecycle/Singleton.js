0f59fd75f6412643e96e5eabc6322b3936ed0729
ben hockey <neonstalwart@gmail.com>
2011-08-15 15:26:04 -0400

    update jshint comment blocks

--- lifecycle/Singleton.js
+++ lifecycle/Singleton.js
@@ -4,11 +4,13 @@
  * Available via the new BSD License.
  */
 /*jshint
-	bitwise: false, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, maxlen: 100,
-	newcap: true, noarg: true, noempty: true, onevar: true, passfail: false, strict: true,
-	undef: true, white: true
+	asi: false, bitwise: false, boss: false, curly: true, eqeqeq: true, eqnull: false, es5: true,
+	evil: false, expr: true, forin: true, globalstrict: false, immed: true, indent: 4, latedef: true,
+	laxbreak: false, loopfunc: true, maxlen: 100, newcap: true, noarg: true, noempty: true,
+	nonew: true, nomen: false, onevar: true, passfail: false, plusplus: false, shadow: false,
+	strict: false, sub: false, trailing: true, undef: true, white: true
 */
-/*global define: false, require: false */
+/*global define: false, require: false*/
 
 define([
 	'../support/compose',
